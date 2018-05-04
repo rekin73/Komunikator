d2469490fd81ab6e6e61b0524be71cfcfc0d3c1d
rekin73 <runefor99@gmail.com>
2018-05-05 00:28:22 +0200

    Lekkie zmiany

--- alp.php
+++ alp.php
@@ -1,3 +1,13 @@
 <?php
-
+$time=time();
+$data=$_REQUEST; //- dana przed wejście w pętlę
+while(time()-$time<=5){
+        // sprawdzam czy $data dana się zmieniła
+        //$data="";
+        if($data!=""){
+                 echo json_encode($data);
+                break;
+        }
+        usleep(25000);
+}
 ?>
