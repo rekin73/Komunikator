b5a5c89fabf6a27b1236baf2b842159dc4e7ff2d
rekin73 <runefor99@gmail.com>
2018-05-05 16:08:54 +0200

    Napraw połączenie

--- alp.php
+++ alp.php
@@ -1,12 +1,33 @@
 <?php
+class RespondObject
+{
+
+        function __Construct($Action,$Context){
+$this->action=$Action;
+        }
+        
+        public $action;
+};
+
 $time=time();
 $data=$_REQUEST; //- dana przed wejście w pętlę
-while(time()-$time<=5){
+while(time()-$time<=1){
         // sprawdzam czy $data dana się zmieniła
         //$data="";
         if($data!=""){
-                 echo json_encode($data);
-                break;
+                $obj=new RespondObject("asdfgh","asqw");
+                echo $time;
+                 switch ($data->action) {
+                        case '_MESSAGE':
+                                echo json_encode($data);
+                                break;
+                        
+                        default:
+                               // echo json_encode();
+                                break;
+                } 
+                 /* echo json_encode($data);
+                break; */
         }
         usleep(25000);
 }
